51bb9323f981b21822f9317988aa5853c0d0a084
ViktorSvertoka <victor.svertoka@gmail.com>
2024-12-17 19:35:48 +0200

    Feat(PY) Change contacts

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlalchemy import select, func, and_, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactModel
 
 
@@ -12,10 +12,11 @@ class ContactRepository:
         self.db = session
 
     async def get_contacts(
-        self, name: str, surname: str, email: str, skip: int, limit: int
+        self, name: str, surname: str, email: str, skip: int, limit: int, user: User
     ) -> List[Contact]:
         stmt = (
             select(Contact)
+            .filter_by(user=user)
             .where(Contact.name.contains(name))
             .where(Contact.surname.contains(surname))
             .where(Contact.email.contains(email))
@@ -25,22 +26,22 @@ class ContactRepository:
         contacts = await self.db.execute(stmt)
         return contacts.scalars().all()
 
-    async def get_contact_by_id(self, contact_id: int) -> Contact | None:
-        stmt = select(Contact).filter_by(id=contact_id)
+    async def get_contact_by_id(self, contact_id: int, user: User) -> Contact | None:
+        stmt = select(Contact).filter_by(id=contact_id, user=user)
         contact = await self.db.execute(stmt)
         return contact.scalar_one_or_none()
 
-    async def create_contact(self, body: ContactModel) -> Contact:
-        contact = Contact(**body.model_dump(exclude_unset=True))
+    async def create_contact(self, body: ContactModel, user: User) -> Contact:
+        contact = Contact(**body.model_dump(exclude_unset=True), user=user)
         self.db.add(contact)
         await self.db.commit()
         await self.db.refresh(contact)
         return contact
 
     async def update_contact(
-        self, contact_id: int, body: ContactModel
+        self, contact_id: int, body: ContactModel, user: User
     ) -> Contact | None:
-        contact = await self.get_contact_by_id(contact_id)
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             for key, value in body.dict(exclude_unset=True).items():
                 setattr(contact, key, value)
@@ -48,26 +49,29 @@ class ContactRepository:
             await self.db.refresh(contact)
         return contact
 
-    async def remove_contact(self, contact_id: int) -> Contact | None:
-        contact = await self.get_contact_by_id(contact_id)
+    async def remove_contact(self, contact_id: int, user: User) -> Contact | None:
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             await self.db.delete(contact)
             await self.db.commit()
         return contact
 
-    async def is_contact_exists(self, email: str, phone: str) -> bool:
-        query = select(Contact).where(
-            (Contact.email == email) | (Contact.phone == phone)
+    async def is_contact_exists(self, email: str, phone: str, user: User) -> bool:
+        query = (
+            select(Contact)
+            .filter_by(user=user)
+            .where((Contact.email == email) | (Contact.phone == phone))
         )
         result = await self.db.execute(query)
         return result.scalars().first() is not None
 
-    async def get_upcoming_birthdays(self, days: int) -> list[Contact]:
+    async def get_upcoming_birthdays(self, days: int, user: User) -> list[Contact]:
         today = date.today()
         end_date = today + timedelta(days=days)
 
         query = (
             select(Contact)
+            .filter_by(user=user)
             .where(
                 or_(
                     func.date_part("day", Contact.birthday).between(
