ea72eb4a53b59dcfe32b4db9c6b312ed5f8bc5d9
ViktorSvertoka <victor.svertoka@gmail.com>
2024-12-18 16:25:42 +0200

    Update config.py

--- src/conf/config.py
+++ src/conf/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DB_URL: str
+
     JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
     JWT_EXPIRATION_SECONDS: int = 3600
@@ -19,9 +20,9 @@ class Settings(BaseSettings):
     USE_CREDENTIALS: bool = True
     VALIDATE_CERTS: bool = True
 
-    CLD_NAME: str
-    CLD_API_KEY: int
-    CLD_API_SECRET: str
+    CLOUDINARY_NAME: str
+    CLOUDINARY_API_KEY: int
+    CLOUDINARY_API_SECRET: str
 
     model_config = ConfigDict(
         extra="ignore", env_file=".env", env_file_encoding="utf-8", case_sensitive=True
